abf121e26f24a6162368419f31fae01df33474db
Type-32 <wilsonsu2008@hotmail.com>
2023-03-01 21:03:09 +0800

    .

--- basic-prefab-project-app/src/App.vue
+++ basic-prefab-project-app/src/App.vue
@@ -1,26 +1,15 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import { Icon } from '@vicons/utils'
-import { NButton, NDivider, NAvatar } from 'naive-ui'
+import { NButton, NLayout, NLayoutContent, NLayoutHeader, NMenu } from 'naive-ui'
 import { reactive } from 'vue'
-
-const counter = reactive({
-  count: 0
-})
-console.log(counter.count)
-function addCounter(amount: any) {
-  counter.count += amount
-}
+import NavigationBar from './components/NavigationBar.vue'
 </script>
 
 <template>
-  <header>
-    <n-space> <n-avatar src="@sicons/fluent/User.svg" size="small" /> <n-divider /> </n-space>
-  </header>
-  <body>
-    <n-button @click="addCounter(1)">Click To Add!</n-button>
-    <p>{{ counter.count }}</p>
-  </body>
+  <main>
+    <NavigationBar />
+  </main>
 </template>
 
 <style scoped></style>
